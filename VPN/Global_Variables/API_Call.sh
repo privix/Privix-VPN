739898278cd789ac849eb0f9fe32f4118a1209cb
TFinch <travisfinch01@gmail.com>
2019-08-10 18:32:29 -0400

    Put logs back in

--- VPN/Global_Variables/API_Call.sh
+++ VPN/Global_Variables/API_Call.sh
@@ -7,26 +7,28 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# Logs for proof of Terms of Service.
+# Logs API Call.
 LOG_FILE="/etc/openvpn/api_call_log.txt"
 LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
 EXTIP=`curl -s4 icanhazip.com`
 
 MNADDY=$(</etc/openvpn/payment_address.txt)
-echo -e ${LOGTIME} " : ${MNADDY}" >> ${LOG_FILE}
+# Only used for testing logs.
+#echo -e ${LOGTIME} " : ${MNADDY}" >> ${LOG_FILE}
 MNSTAT=$(curl -v "https://explorer.privix.io/api/masternode/${MNADDY}" | jq ".mns.status")
-echo -e ${LOGTIME} " : ${MNSTAT}" >> ${LOG_FILE}
+# Only used for testing logs.
+#echo -e ${LOGTIME} " : ${MNSTAT}" >> ${LOG_FILE}
 
-#if [ $MNSTAT == "ENABLED" ]; then
+if [ $MNSTAT == "ENABLED" ]; then
 # Make the logfile input for ENABLED
-#echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${GREEM}${MNSTAT}${NC}." >> ${LOG_FILE}
+echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${GREEM}${MNSTAT}${NC}." >> ${LOG_FILE}
 
-#elif [ $MNSTAT == "EXPIRED" ]; then
+elif [ $MNSTAT == "EXPIRED" ]; then
 # Make the logfile input EXPIRED
-#echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${RED}${MNSTAT}${NC}." >> ${LOG_FILE}
+echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${RED}${MNADDY}${NC} as their masternode address with a node status of: ${RED}${MNSTAT}${NC}." >> ${LOG_FILE}
 
-#else
+else
 # Make the logfile input for anything else
-#echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${RED}${MNSTAT}${NC}." >> ${LOG_FILE}
+echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${RED}${MNSTAT}${NC}." >> ${LOG_FILE}
 
-#fi
+fi
