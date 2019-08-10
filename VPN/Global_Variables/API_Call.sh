45dc479ed67a7740c2e4d36a2879a335601a9cd4
TFinch <travisfinch01@gmail.com>
2019-08-10 18:56:06 -0400

    Remove unwanted logs

--- VPN/Global_Variables/API_Call.sh
+++ VPN/Global_Variables/API_Call.sh
@@ -13,11 +13,8 @@ LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
 EXTIP=`curl -s4 icanhazip.com`
 
 MNADDY=$(</etc/openvpn/payment_address.txt)
-# Only used for testing logs.
-#echo -e ${LOGTIME} " : ${MNADDY}" >> ${LOG_FILE}
+
 MNSTAT=$(curl -v "https://explorer.privix.io/api/masternode/${MNADDY}" | jq ".mns.status")
-# Only used for testing logs.
-#echo -e ${LOGTIME} " : ${MNSTAT}" >> ${LOG_FILE}
 
 if [ $MNSTAT == "ENABLED" ]; then
 # Make the logfile input for ENABLED
@@ -25,6 +22,6 @@ echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has
 
 else
 # Make the logfile input for anything else
-echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${RED}${MNADDY}${NC} as their masternode address with a node status of: ${RED}${MNSTAT}${NC}." >> ${LOG_FILE}
+echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${RED}${MNSTAT}${NC}." >> ${LOG_FILE}
 
 fi
