d23430f9079a97622f1dc071522b0719e35a431b
TFinch <travisfinch01@gmail.com>
2019-08-10 23:41:46 -0400

    Change Color to not always be green

--- VPN/Checks/MN_Check.sh
+++ VPN/Checks/MN_Check.sh
@@ -25,7 +25,7 @@ MNSTAT=$(</etc/openvpn/masternode_status.txt)
 		exit 1
 	fi
     
-    echo "${GREEN}Checking Masternode Status${GREEN}"
+    echo "${GREEN}Checking Masternode Status${NC}"
 
 if [ $MNSTAT == "ENABLED" ]; then
 echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${GREEM}${MNSTAT}${NC}." >> ${LOG_FILE}
