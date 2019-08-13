6d8e86fa63f0b21647ba8081dee8f06af785dfa4
TFinch <travisfinch01@gmail.com>
2019-08-12 21:47:11 -0400

    Test Cronjob

--- VPN/Checks/Cron.sh
+++ VPN/Checks/Cron.sh
@@ -11,10 +11,12 @@ NC='\033[0m' # No Color
 LOG_FILE="/etc/privix/mn_check_log.txt"
 LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
 EXTIP="$(ip route get 1 | awk '{print $NF;exit}')"
-CRON_PATH="/etc/privix/mn_cron"
+CRON_PATH="/etc/privix/mn_cron.cron"
 
 
     echo -e "${GREEN}Creating Crontab Entry${NC}"
 	echo  "*/10 * * * * cd privix-vpn/Checks/active_check.sh" >> $CRON_PATH
+	crontab $CRON_PATH
 	echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has just created the CronJob." >> ${LOG_FILE}
-
+		cd -
+	fi
