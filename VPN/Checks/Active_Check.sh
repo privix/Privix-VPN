8cc4d12cba3b76728a7771fdd4019a30d84ca042
TFinch <travisfinch01@gmail.com>
2019-08-13 20:07:38 -0400

    Else do

--- VPN/Checks/Active_Check.sh
+++ VPN/Checks/Active_Check.sh
@@ -19,9 +19,11 @@ MNSTAT=$(</etc/openvpn/masternode_status.txt)
 
 	if [[ $MNSTAT == "ENABLED" ]]; then
 		echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${GREEM}${MNSTAT}${NC}." >> ${LOG_FILE}
-        exit 1
+        sleep 5
+		exit 1
         fi
-    else  
+    else
+    do 
 		echo -e ${LOGTIME} " : User ${GREEN}${USER}${NC} on vps ${BLUE}${EXTIP}${NC} has provided ${GREEN}${MNADDY}${NC} as their masternode address with a node status of: ${GREEM}${MNSTAT}${NC}." >> ${LOG_FILE}
 			rm -rf /etc/privix/mn_cron.cron
 			systemctl stop privix-vpn-server
